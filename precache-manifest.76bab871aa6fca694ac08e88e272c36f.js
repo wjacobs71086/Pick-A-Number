f621c916616b05933371c2dab66fda3819a67b76
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-10-11 15:03:14 -0700

    Updates

---
+++ precache-manifest.76bab871aa6fca694ac08e88e272c36f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc1ee307db9640bb325ae77ce32e0cf",
+    "url": "/reactbookmarkapp/index.html"
+  },
+  {
+    "revision": "da6bee045c0fa9860c53",
+    "url": "/reactbookmarkapp/static/css/main.b9d6ebbc.chunk.css"
+  },
+  {
+    "revision": "00b958198e7937f95df0",
+    "url": "/reactbookmarkapp/static/js/2.ed23b9cf.chunk.js"
+  },
+  {
+    "revision": "da6bee045c0fa9860c53",
+    "url": "/reactbookmarkapp/static/js/main.99e2add7.chunk.js"
+  },
+  {
+    "revision": "f8d6c31a26399f5b6d19",
+    "url": "/reactbookmarkapp/static/js/runtime-main.b089b177.js"
+  }
+]);
